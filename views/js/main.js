be9d21ed229385a5abc322c34276c83c156ddd24
T Loi <tloi@hotmail.com>
2016-07-30 02:35:14 -0400

    jank

--- views/js/main.js
+++ views/js/main.js
@@ -502,9 +502,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  var r = Math.sin((document.body.scrollTop / 1250));
   for (var i = 0, max=items.length; i < max; i++) {
-
+      var phase = r + (i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
